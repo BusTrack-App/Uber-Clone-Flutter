d3eed1efe6f294ad237659f24a459a7cc1cd402a
Juan Mera <juan1mera2barrera@gmail.com>
2025-11-04 17:41:08 +0300

    feat: Manejo de respuestas y errores

--- lib/src/data/dataSource/remote/services/auth_service.dart
+++ lib/src/data/dataSource/remote/services/auth_service.dart
@@ -3,9 +3,13 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:uber_clone/src/data/api/api_config.dart';
+import 'package:uber_clone/src/domain/models/auth_response.dart';
+import 'package:uber_clone/src/domain/models/user.dart';
+import 'package:uber_clone/src/domain/utils/list_to_string.dart';
+import 'package:uber_clone/src/domain/utils/resource.dart';
 
 class AuthService {
-  Future<dynamic> login(String email, String password) async {
+  Future<Resource<AuthResponse>> login(String email, String password) async {
     try {
       Uri url = Uri.http(ApiConfig.API_PROJECT, '/auth/login');
       Map<String, String> headers = {'Content-Type': 'application/json'};
@@ -14,9 +18,39 @@ class AuthService {
       debugPrint('Response: $response');
       final data = json.decode(response.body);
       debugPrint('Data: $data');
-      return data;
+      debugPrint('Status Coode: ${response.statusCode}');
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        AuthResponse authResponse = AuthResponse.fromJson(data);
+        debugPrint('Data Remote: ${authResponse.toJson()}');
+        debugPrint('Token: ${authResponse.token}');
+        return Success(authResponse);
+      } else {
+        return ErrorData(listToString(data['message']));
+      }
     } catch (e) {
-      debugPrint('Error $e');
+      debugPrint('Error: $e');
+      return ErrorData(e.toString());
+    }
+  }
+
+  Future<Resource<AuthResponse>> register(User user) async {
+    try {
+      Uri url = Uri.http(ApiConfig.API_PROJECT, '/auth/register');
+      Map<String, String> headers = {'Content-Type': 'application/json'};
+      String body = json.encode(user);
+      final response = await http.post(url, headers: headers, body: body);
+      final data = json.decode(response.body);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        AuthResponse authResponse = AuthResponse.fromJson(data);
+        debugPrint('Data Remote: ${authResponse.toJson()}');
+        debugPrint('Token: ${authResponse.token}');
+        return Success(authResponse);
+      } else {
+        return ErrorData(listToString(data['message']));
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+      return ErrorData(e.toString());
     }
   }
 }
