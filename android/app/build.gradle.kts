92caf113ae5f7b45cebd05d8d58e6f250ce0d60e
Juan Mera <juan1mera2barrera@gmail.com>
2025-11-05 15:50:11 +0300

    build: Permisos de ubicacion para Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,25 +7,24 @@ plugins {
 
 android {
     namespace = "com.example.uber_clone"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+    // Forzar compileSdk y ndkVersion para evitar warnings de plugins
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.uber_clone"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion  // Ej: 21
+        targetSdk = 36                  // Recomendado igual que compileSdk
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
