6a84ddb4627156d7ce7b36d8f2ba9826ea5af9e5
Juan Mera <juan1mera2barrera@gmail.com>
2025-11-06 17:23:59 +0300

    err: HAY UN ERROR AL RECARGAR EL MAPA

--- lib/src/presentation/screens/client/map_seeker/client_map_seecker_screen.dart
+++ lib/src/presentation/screens/client/map_seeker/client_map_seecker_screen.dart
@@ -13,6 +13,7 @@ class ClientMapSeeckerScreen extends StatefulWidget {
 }
 
 class ClientMapSeeckerScreenState extends State<ClientMapSeeckerScreen> {
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
