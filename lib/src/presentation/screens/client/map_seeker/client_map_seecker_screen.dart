fd325459b9f96f54fadc51d1e46a1d24becb0921
Juan Mera <juan1mera2barrera@gmail.com>
2025-11-10 17:02:30 +0300

    fix: Eliminando warnings

--- lib/src/presentation/screens/client/map_seeker/client_map_seecker_screen.dart
+++ lib/src/presentation/screens/client/map_seeker/client_map_seecker_screen.dart
@@ -60,6 +60,7 @@ class ClientMapSeeckerScreenState extends State<ClientMapSeeckerScreen> {
                     // Actualizar el texto solo si no hay un pickUpDescription ya seleccionado
                     if (state.pickUpDescription.isEmpty && state.placemarkData != null) {
                       pickUpController.text = state.placemarkData?.address ?? '';
+                      // ignore: use_build_context_synchronously
                       context.read<ClientMapSeekerBloc>().add(OnAutoCompletedPickUpSelected(
                         lat: state.placemarkData!.lat, 
                         lng: state.placemarkData!.lng,
@@ -72,6 +73,7 @@ class ClientMapSeeckerScreenState extends State<ClientMapSeeckerScreen> {
                   _isSelectingFromAutocomplete = false;
                 },
                 onMapCreated: (GoogleMapController controller) {
+                  // ignore: deprecated_member_use
                   controller.setMapStyle(
                     '[ { "featureType": "all", "elementType": "labels.text.fill", "stylers": [ { "color": "#ffffff" } ] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [ { "color": "#000000" }, { "lightness": 13 } ] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 } ] }, { "featureType": "landscape", "elementType": "all", "stylers": [ { "color": "#08304b" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#0c4152" }, { "lightness": 5 } ] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b434f" }, { "lightness": 25 } ] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b3d51" }, { "lightness": 16 } ] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [ { "color": "#000000" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "color": "#146474" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "color": "#021019" } ] } ]',
                   );
